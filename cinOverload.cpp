43b7c1d323202e6940ba31f4210a206f2babac2c
Yahya Mammadli <yahya.mammadli07@gmail.com>
2025-11-29 15:16:27 +0000

    I DO IT!

--- cinOverload.cpp
+++ cinOverload.cpp
@@ -4,213 +4,128 @@
 
 using namespace std;
 
-
-
-
-
-
-
-
-istream& operator>>(istream& is, int& value) 
+namespace my
 {
-    
-    ios_base::iostate state = ios_base::goodbit;
-
-    
-    char ch;
-    
-    while (is.get(ch) && isspace(ch)) 
+    istream& operator>>(istream& is, int& value)
     {
+        ios_base::iostate state = ios_base::goodbit;
+        char ch;
+
         
-    }
-
-    
-    if (!is.good()) 
-    {
-        if (is.eof()) 
+        while (is.get(ch) && isspace(ch))
         {
-            state |= ios_base::eofbit;
         }
-       
-        state |= ios_base::failbit;
-        
-        is.setstate(state);
-        
-        return is;
-    }
 
-   
-    bool negative = false;
-    
-    if (ch == '-') 
-    {
-        negative = true;
-       
-        if (!is.get(ch)) 
+        if (!is.good())
         {
-        
-            state |= ios_base::failbit;
-            
-            is.setstate(state);
-            
-            return is;
-        }
-    
-    }
-    
-    
-    
-    else if (ch == '+') 
-    {
-        
-        if (!is.get(ch)) 
-        {
-            state |= std::ios_base::failbit;
-        
-            is.setstate(state);
-            
-            return is;
-        
-        }
-    }
-
-   
-   
-    
-    if (!isdigit(static_cast<unsigned char>(ch)))
-    {
-        
-        is.putback(ch);
-        
-        if (negative)
-        {
-            is.putback('-');
-        }
-        
-        state |= std::ios_base::failbit;
-        
-        is.setstate(state);
-        
-        return is;
-    }
-
-
-
-
-
-    
-    long long result = 0;  
-
-    int digits_count = 0;
-
-
-
-
-
-
-    do 
-    {
-        
-        int digit = ch - '0';
-
-        
-        if (result > (numeric_limits<int>::max() - digit) / 10)
-        {
+            if (is.eof())
+            {
+                state |= ios_base::eofbit;
+            }
             state |= ios_base::failbit;
             is.setstate(state);
             return is;
         }
 
-        result = result * 10 + digit;
-        digits_count++;
+        bool negative = false;
+        if (ch == '-')
+        {
+            negative = true;
+            if (!is.get(ch))
+            {
+                state |= ios_base::failbit;
+                is.setstate(state);
+                return is;
+            }
+        }
+        else if (ch == '+')
+        {
+            if (!is.get(ch))
+            {
+                state |= ios_base::failbit;
+                is.setstate(state);
+                return is;
+            }
+        }
 
-    } while (is.get(ch) && isdigit(static_cast<unsigned char>(ch)));
-
-  
-
-
-
-
-    if (digits_count > 0)
-    {
-        
-        if (is.good()) 
+        if (!isdigit(static_cast<unsigned char>(ch)))
         {
             is.putback(ch);
-        }
-
-        
-        if (negative)
-        {
-            result = -result;
-        }
-
-        
-        if (result < numeric_limits<int>::min() || result > numeric_limits<int>::max())
-        {
-
+            if (negative)
+            {
+                is.putback('-');
+            }
             state |= ios_base::failbit;
-
             is.setstate(state);
-
             return is;
-
         }
 
-        value = static_cast<int>(result);
+        long long result = 0;
+        int digits_count = 0;
+
+        do
+        {
+            int digit = ch - '0';
+
+            if (result > (numeric_limits<int>::max() - digit) / 10)
+            {
+                state |= ios_base::failbit;
+                is.setstate(state);
+                return is;
+            }
+
+            result = result * 10 + digit;
+            digits_count++;
+
+        } while (is.get(ch) && isdigit(static_cast<unsigned char>(ch)));
+
+        if (digits_count > 0)
+        {
+            if (is.good())
+            {
+                is.putback(ch);
+            }
+
+            if (negative)
+            {
+                result = -result;
+            }
+
+            if (result < numeric_limits<int>::min() || result > numeric_limits<int>::max())
+            {
+                state |= ios_base::failbit;
+                is.setstate(state);
+                return is;
+            }
+
+            value = static_cast<int>(result);
+        }
+        else
+        {
+            state |= ios_base::failbit;
+            is.setstate(state);
+        }
+
+        return is;
     }
-
-
-    else 
-    {
-        
-        state |= ios_base::failbit;
-        
-        is.setstate(state);
-    }
-
-  
-    
-    
-    
-    return is;
-
-
-
 }
-
-
-
-
-// I was curious about how the cin function works, but it turned out that cin is just an object of the istream class, which has 16 overloads of the ">>" operator 
-// I wrote my own overload of "operator>>", but the compiler does not allow me to implement it due to a priority conflict and the error 'operator >>' is ambiguous 
-// But the experience was interesting
 
 int main()
 {
     int number;
 
+    cout << "Type anything => ";
 
-    if (std::cin >> number)
+    if (my::operator>>(std::cin, number))
     {
-        cout << "You type => " << number << endl;
+        cout << "\nYou typed => " << number << endl;
     }
-
-
-
-    else 
+    else
     {
-        cout << "Erorr!!!" << endl;
-
-        
+        cout << "Error!!!" << endl;
         cin.clear();
-    
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
-    
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
-
-
-    
 
     return 0;
 }
